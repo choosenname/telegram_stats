4fe04d1e942ec95d1aa944ff4f78c3a17a112aa1
choosenname <vlad@luckashonok.ru>
2024-12-27 16:48:40 +0300

    feat: impl dyn reader

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,24 @@
-use sqlx::PgPool;
 use crate::data::implementation::postgres::Postgres;
 use crate::services::data_processor::DataProcessor;
+use sqlx::PgPool;
 
-mod services;
-mod data;
-mod utils;
 mod core;
+mod data;
+mod services;
+mod utils;
 
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().unwrap();
-    
-    let data_repository = Postgres{
+
+    let data_repository = Postgres {
         pool: PgPool::connect(env!("DATABASE_URL")).await.unwrap(),
     };
 
     let processor = DataProcessor::new(data_repository);
 
-    processor.process_file("/home/vlad/Dowloads/tg/result.json").await.unwrap();
-
+    processor
+        .process_file_from_reader("/home/vlad/Dowloads/tg/result.json")
+        .await
+        .unwrap();
 }
--- src/services/data_processor.rs
+++ src/services/data_processor.rs
@@ -1,6 +1,9 @@
+use std::error::Error;
 use crate::data::repositories::data_repository::DataRepository;
 use serde::de::DeserializeOwned;
 use std::fs;
+use std::fs::File;
+use std::io::BufReader;
 
 pub struct DataProcessor<D: DataRepository> {
     pub data_repository: D,
@@ -14,9 +17,17 @@ where
         Self { data_repository }
     }
 
-    pub async fn process_file(&self, file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    pub async fn process_file_from_str(&self, file_path: &str) -> Result<(), Box<dyn Error>> {
         let file_content = fs::read_to_string(file_path)?;
         let data: D::Data = serde_json::from_str(&file_content)?;
+        self.data_repository.save(data).await?;
+        Ok(())
+    }
+
+    pub async fn process_file_from_reader(&self, file_path: &str) -> Result<(), Box<dyn Error>> {
+        let file = File::open(file_path)?;
+        let reader = BufReader::new(file);
+        let data: D::Data = serde_json::from_reader(reader)?;
         self.data_repository.save(data).await?;
         Ok(())
     }
